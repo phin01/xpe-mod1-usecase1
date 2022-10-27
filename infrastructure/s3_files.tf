1c70c9ec1f698b4da061baef8e0b15341083e113
phin01 <phin01@gmail.com>
2022-10-27 20:48:41 -0300

    add spark file to bucket

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -28,3 +28,13 @@ resource "aws_s3_object" "delta_upsert" {
   etag = filemd5("./etl/02_delta_spark_upsert.py")
 
 }
+
+resource "aws_s3_bucket_object" "job_spark" {
+
+  bucket = aws_s3_bucket.datalake.id
+  key    = "emr-code/pyspark/job_spark_from_tf.py"
+  acl    = "private"
+  source = "../job_spark.py"
+  etag   = filemd5("../job_spark.py")
+  
+}
